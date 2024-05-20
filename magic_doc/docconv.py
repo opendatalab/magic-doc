74614ba733f4832c03c7a19949bef5d429f3c6d1
徐超 <xuchao@pjlab.org.cn>
2024-05-20 16:06:20 +0800

    add timeout when convert doc

--- magic_doc/docconv.py
+++ magic_doc/docconv.py
@@ -16,21 +16,23 @@ class S3Config(object):
     
 
 class DocConverter(object):
-    def __init__(self, s3_config:S3Config, temp_dir:str='/tmp/'):
+    def __init__(self, s3_config:S3Config, temp_dir:str='/tmp/', conv_timeout=60):
         """
         初始化一次，多次调用convert方法。避免模型加载和构造s3client的性能开销。
         """
         self.__s3cfg = s3_config
         if self.__s3cfg:
             self.__s3cli = S3Client(self.__s3cfg) # TODO
-        
+        self.__temp_dir = temp_dir
+        self.__conv_timeout = conv_timeout # 转换超时时间，单位秒        
 
-    def convert(self, doc_path:str, progress_file_path:str):
+    def convert(self, doc_path:str, progress_file_path:str, conv_timeout=None):
         """
         doc_path: str, path to the document, support local file path and s3 path.
         progress_file_path: str, path to the progress file, support local file path only.
         return markdown string or raise ConvException.
         """
+        conv_timeout = conv_timeout or self.__conv_timeout # 根据这个时间判断函数超时
         markdown_string = ""
         # TODO
         return markdown_string

e1757810d1a41420efb10c6f963bdae2496abb45
赵小蒙 <moe@myhloli.com>
2024-05-23 19:06:38 +0800

    update ppt parse logic

--- magic_doc/conv/ppt_libreoffice.py
+++ magic_doc/conv/ppt_libreoffice.py
@@ -1,9 +1,11 @@
-import subprocess
+import os
+from subprocess import Popen
 import tempfile
 from pathlib import Path
 
 from loguru import logger
 
+from magic_doc.contrib.model import Page
 from magic_doc.contrib.office.pptx_extract import PptxExtractor
 from magic_doc.conv.base import BaseConv
 from magic_doc.progress.pupdator import ConvProgressUpdator
@@ -20,7 +22,7 @@ class Ppt(BaseConv):
             page_content_list = page['content_list']
             total = len(page_content_list)
             for index, content in enumerate(page_content_list):
-                progress = 70 + int(index / total * 30)
+                progress = 80 + int(index / total * 20)
                 # logger.info(f"progress: {progress}")
                 self._progress_updator.update(progress)
                 if content['type'] == 'image':
@@ -30,34 +32,36 @@ class Ppt(BaseConv):
                     md_content_list.append(data)
         return "\n".join(md_content_list)
 
-    def ppt_to_contentlist(self, bits):
+    def ppt_to_pptx(self, ppt_path: str, dir_path: str) -> str:
+        cmd = f'libreoffice --headless --convert-to pptx "{ppt_path}" --outdir "{dir_path}"'
+        logger.info(cmd)
+        process = Popen(cmd, shell=True)
+        process.wait()
+        fname = str(Path(ppt_path).stem)
+        pptx_path = os.path.join(os.path.dirname(ppt_path), f'{fname}.pptx')
+        if not os.path.exists(pptx_path):
+            # logger.error(f"> !!! File conversion failed {ppt_path} ==> {pptx_path}")
+            raise Exception(f"> !!! File conversion failed {ppt_path} ==> {pptx_path}")
+        else:
+            return pptx_path
+
+    def ppt_to_contentlist(self, bits) -> list[Page]:
         with tempfile.TemporaryDirectory() as temp_path:
             temp_dir = Path(temp_path)
             media_dir = temp_dir / "media"
             media_dir.mkdir()
             file_path = temp_dir / "tmp.ppt"
             file_path.write_bytes(bits)
-            pptx_file_path = temp_dir / "tmp.pptx"
-            cmd = f'libreoffice --headless --convert-to pptx:"MS PowerPoint 2007 XML" "{file_path}" --outdir "{temp_dir}"'
-            # python执行命令行
-            result = subprocess.run(
-                [cmd],
-                check=True,
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True  # 确保是字符串而不是字节串
-            )
-            output = result.stdout.rstrip()  # 移除输出末尾的换行符
-            logger.info(output)
-            self._progress_updator.update(40)
+            pptx_file_path = self.ppt_to_pptx(str(file_path), str(temp_path))
+            self._progress_updator.update(50)
             pptx_extractor = PptxExtractor()
             pages = pptx_extractor.extract(pptx_file_path, "tmp", temp_dir, media_dir, True)
-            self._progress_updator.update(70)
+            self._progress_updator.update(80)
             return pages
 
 
 if __name__ == '__main__':
     pupdator = ConvProgressUpdator()
     logger.info(
-        Ppt(pupdator).to_md(open(r"D:\project\20240514magic_doc\doc_ppt\doc\【英文-模板】Professional Pack Standard.ppt", "rb").read()))
+        Ppt(pupdator).to_md(
+            open(r"D:\project\20240514magic_doc\doc_ppt\doc\【英文-模板】Professional Pack Standard.ppt", "rb").read()))

b7a15d64c837cc6f510d5e68d8dcdbb05bd3e2a3
houlinfeng <m15237195947@163.com>
2024-05-29 20:45:09 +0800

    feat: restful api

--- magic_doc/restful_api/api/analysis/magic_pdf_view.py
+++ magic_doc/restful_api/api/analysis/magic_pdf_view.py
@@ -1,8 +1,8 @@
-from flask import request
+from flask import request, current_app
 from flask_restful import Resource
 from marshmallow import ValidationError
 from pathlib import Path
-from magic_doc.pdf_transform import DocConverter
+from magic_doc.pdf_transform import DocConverter, S3Config
 from .serialization import MagicPdfSchema
 from magic_doc.restful_api.common.custom_response import generate_response
 from loguru import logger
@@ -20,6 +20,11 @@ class MagicPdfView(Resource):
         # pdf解析
         file_name = str(Path(pdf_url).stem)
         pf_path = f"/tmp/{file_name}.txt"
-        docconv = DocConverter(None)
+        app_config = current_app.config
+        if pdf_url.startswith("s3://"):
+            s3_config = S3Config(app_config["S3AK"], app_config["S3SK"], app_config["S3ENDPOINT"])
+            docconv = DocConverter(s3_config)
+        else:
+            docconv = DocConverter(None)
         result = docconv.convert_to_mid_result(pdf_url, pf_path, 60)
         return generate_response(data=result, markDownUrl=pdf_url)

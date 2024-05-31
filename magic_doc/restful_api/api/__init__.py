5ab7abafa8a0f96495979aec9b14686a25caaf11
houlinfeng <m15237195947@163.com>
2024-05-31 19:13:58 +0800

    update: refactor log

--- magic_doc/restful_api/api/__init__.py
+++ magic_doc/restful_api/api/__init__.py
@@ -1,9 +1,27 @@
 import os
+from datetime import datetime
+from pathlib import Path
+from loguru import logger
 from .extentions import app, db, migrate, jwt, ma
 from magic_doc.restful_api.common.web_hook import before_request
-from magic_doc.restful_api.common.logger import setup_log
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def init_app_log(config):
+    """
+    Setup logging
+    :param config:  config file
+    :return:
+    """
+    log_path = os.path.join(Path(__file__).parent.parent, "log")
+    if not Path(log_path).exists():
+        Path(log_path).mkdir(parents=True, exist_ok=True)
+    log_level = config.get("LOG_LEVEL")
+    log_name = f'log_{datetime.now().strftime("%Y-%m-%d")}.log'
+    log_file_path = os.path.join(log_path, log_name)
+    logger.add(str(log_file_path), rotation='00:00', encoding='utf-8', level=log_level, enqueue=True)
+    return logger
 
 
 def _register_db(flask_app):
